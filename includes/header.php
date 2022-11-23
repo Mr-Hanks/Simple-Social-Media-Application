8292fa507f9d9ebdafe3cdded5a60babbe0476a4
Collin Hanks <collinnhankss@gmail.com>
2022-11-22 23:53:55 -0600

    Update

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap js -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!--<script src="assets/js/bootbox.min.js"></script> -->
-    <script src="assets/js/facebook.js"></script>
+    <script src="assets/js/social.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js" integrity="sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     
     
