2f4a8623b81d53faeb176eba49e2bc5b71172fc9
Collin Hanks <collinnhankss@gmail.com>
2022-11-26 17:49:00 -0600

    update

--- profile.php
+++ profile.php
@@ -154,8 +154,8 @@ if (isset($_GET['profile_username'])) {
             return (
                 rect.top >= 0 &&
                 rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
         }
     });
