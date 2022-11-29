b929cce0555ffceef9df5ff178c7953d47bb2a89
Collin Hanks <collinnhankss@gmail.com>
2022-11-28 22:13:07 -0600

    update

--- includes/form_handlers/deletePost.php
+++ includes/form_handlers/deletePost.php
@@ -5,7 +5,7 @@
         $post_id = $_GET['post_id'];
     }
 
-    // Included in bootstrap 
+    
     if (isset($_POST['result'])) {
         if ($_POST['result'] == 'true')
             $query = mysqli_query($con, "DELETE FROM posts WHERE id='$post_id'");
